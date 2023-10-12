d3da8c07b8d47c2954fbdbf19b1656e894cdee0c
hpcatta <147749730+hpcatta@users.noreply.github.com>
2023-10-12 23:34:43 +0530

    cuda_installation
    cuda_installation

---
+++ cuda_installation.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Update the system and install basic utilities
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y build-essential software-properties-common
+
+# Install GNU GCC and Fortran compilers
+sudo apt install -y gcc g++ gfortran
+
+# Install LLVM
+sudo apt install -y llvm
+
+# Install BLAS and LAPACK
+sudo apt install -y libblas-dev liblapack-dev
+
+# Install FFTW
+sudo apt install -y libfftw3-dev
+
+# Install ScalAPACK (this will also install openMPI as a dependency)
+sudo apt install -y libscalapack-mpi-dev
+
+# Install NVIDIA CUDA (assuming you are using Ubuntu 20.04)
+# For other versions of Ubuntu, you might need a different repository
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
+sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub
+sudo add-apt-repository "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
+sudo apt update
+sudo apt install -y cuda
+
+# After installing, you might want to add CUDA to PATH and LD_LIBRARY_PATH in your .bashrc or .zshrc:
+# export PATH=/usr/local/cuda/bin:${PATH}
+# export LD_LIBRARY_PATH=/usr/local/cuda/lib64:${LD_LIBRARY_PATH}
+
+# Print completion message
+echo "Installation complete."
+
