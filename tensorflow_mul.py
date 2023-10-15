2ff9e57102c59ec7c6f1f98f1c6c06609a72d0c5
hpcatta <147749730+hpcatta@users.noreply.github.com>
2023-10-16 00:16:19 +0530

    tensorflow_mul
    tensorflow_mul

--- tensorflow_mul.py
+++ tensorflow_mul.py
@@ -1,21 +1,17 @@
 import tensorflow as tf
 
-# Check if GPU is available
+print("=== TensorFlow ===")
+
 if tf.config.list_physical_devices('GPU'):
-    print("GPU is available!")
+    print("GPU is available for TensorFlow!")
 else:
-    print("GPU is not available. The following code will run on CPU.")
+    print("GPU is not available for TensorFlow. The following code will run on CPU.")
 
 # Create two random matrices
-A = tf.random.normal([1000, 1000])
-B = tf.random.normal([1000, 1000])
+A_tf = tf.random.normal([1000, 1000])
+B_tf = tf.random.normal([1000, 1000])
 
 # Matrix multiplication
-C = tf.matmul(A, B)
-
-# Run a TensorFlow session to perform the multiplication
-with tf.compat.v1.Session() as sess:
-    result = sess.run(C)
-
-print("Matrix multiplication complete!")
+C_tf = tf.matmul(A_tf, B_tf)
+print("TensorFlow matrix multiplication complete!")
 
